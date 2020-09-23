292f3357c513b21c0be866920e74f1204d6d3e64
Joao Soutto <joaosoutto@hotmail.com>
2020-09-23 19:15:46 -0300

    subindo

--- src/components/Encomendas/Encomendas.jsx
+++ src/components/Encomendas/Encomendas.jsx
@@ -9,99 +9,74 @@ const Encomendas = () => {
   return (
     <section className={styles.section} id="Encomendas">
       <div className={styles.left}>
-        <div className={styles.upper}>
-          <img className={styles.logo} src={encomendas} alt="encomendas logo" />
-          <h1 className={styles.titleL}>ENCOMENDAS</h1>
-        </div>
+        <div className={styles.first}>
+          <div className={styles.upper}>
+            <img
+              className={styles.logo}
+              src={encomendas}
+              alt="encomendas logo"
+            />
 
-        <div className={styles.down}>
-          <h1 className={styles.titleLL}>Como fazer seu pedido:</h1>
-
-          <div className={styles.aaa}>
-            <div className={styles.number}>
-              <h1 className={styles.numberin}>1</h1>
-            </div>
-            <p className={styles.par}>
+            <h1 className={styles.titleL}>ENCOMENDAS</h1>
+          </div>
+          <div className={styles.down}>
+            <p className={styles.pp}>
               Toda nossa produção é artesanal, por isso, trabalhamos sob
-              encomendas
+              encomendas!
             </p>
-          </div>
 
-          <div className={styles.aaa}>
-            <div className={styles.number}>
-              <h1 className={styles.numberin}>2</h1>
-            </div>
-            <p className={styles.par}>
-              Através do menu, escolha nossos produtos que deseja encomendar
-            </p>
-          </div>
-
-          <div className={styles.aaa}>
-            <div className={styles.number}>
-              <h1 className={styles.numberin}>3</h1>
-            </div>
-            <p className={styles.par}>
-              Clique no link do whatsapp disponível na aba de contatos e entre
-              em contato conosco!
-            </p>
-          </div>
-
-          <div className={styles.aaa}>
-            <div className={styles.number}>
-              <h1 className={styles.numberin}>4</h1>
-            </div>
-            <p className={styles.par}>
-              Informe seu pedido, a data de entrega, se deseja retirar ou
-              receber por delivery e qual será a forma de pagamento{' '}
+            <p className={styles.pp}>
+              Para fazer a sua, basta escolher os produtos que deseja
+              encomendar, informar a forma de pagamento (trabalhamos com
+              dinheiro, transferência, PIC PAY, ou boleto a partir de R$20,00),
+              e nos dizer se deseja retirar no local ou se deseja a opção de
+              delivery.{' '}
             </p>
           </div>
         </div>
 
-        <div className={styles.down}>
-          <h1 className={styles.titleLL}>Formas de Pagamento:</h1>
+        <div className={styles.first}>
+          <div className={styles.upper}>
+            <img className={styles.logo} src={entregas} alt="entregas logo" />
 
-          <p className={styles.par}>
-            Você pode pagar em <span className={styles.forma}>dinheiro</span>,{' '}
-            <span className={styles.forma}>transferência</span>,{' '}
-            <span className={styles.forma}>PIC PAY</span>, ou{' '}
-            <span className={styles.forma}>boleto</span> (a partir de R$20,00).
-          </p>
+            <h1 className={styles.titleL}>ENTREGAS</h1>
+          </div>
+          <div className={styles.down}>
+            <p className={styles.pp}>
+              Nossas entregas funcionam de duas formas:
+            </p>
+
+            <h3 className={styles.entrega}>Retirada:</h3>
+            <p className={styles.pp}>
+              Você pode retirar seu pedido no centro de BH, pertinho do Shopping
+              Cidade! Basta combinar dia e horário.
+            </p>
+            <h3 className={styles.entrega}>Delivery:</h3>
+            <p className={styles.pp}>
+              Entregamos às sextas a partir das 18H e aos sábados! (situações
+              especiais podem ser combinadas).
+            </p>
+            <p className={styles.pp}>
+              O preço varia de acordo com o tamanho do pedido e do bairro.
+            </p>
+          </div>
         </div>
       </div>
 
       <div className={styles.right}>
-        <div className={styles.upper}>
-          <img className={styles.logo} src={entregas} alt="encomendas logo" />
-          <h1 className={styles.titleR}>ENTREGAS</h1>
-        </div>
+        <h1 className={styles.rtitle}>E aí, gostou?</h1>
+        <p className={styles.rp}>
+          Para realizar seu pedido é fácil! Basta clicar no botão abaixo para
+          entrar em contato conosco e trazer um pouco mais de afeto para o seu
+          dia!
+        </p>
 
-        <div className={styles.down}>
-          <h1 className={styles.titleRR}>
-            Nossas entregas funcionam de duas formas:
-          </h1>
-        </div>
-
-        <div className={styles.down}>
-          <h1 className={styles.titleRR}>RETIRADA:</h1>
-
-          <p className={styles.par}>
-            Você pode retirar seu pedido no centro de BH, pertinho do Shopping
-            Cidade!
-          </p>
-          <p className={styles.par}>Basta combinar dia e horário. </p>
-        </div>
-
-        <div className={styles.down}>
-          <h1 className={styles.titleRR}>DELIVERY:</h1>
-
-          <p className={styles.par}>
-            Entregamos às sextas a partir das 18H e aos sábados! (situações
-            especiais podem ser combinadas).
-          </p>
-          <p className={styles.par}>
-            O preço varia de acordo com o tamanho do pedido e do bairro.{' '}
-          </p>
-        </div>
+        <a
+          href="https://api.whatsapp.com/send?phone=5531997737633"
+          target="_blank"
+        >
+          <button className={styles.btn}>EU QUERO AFETO</button>
+        </a>
       </div>
     </section>
   );
